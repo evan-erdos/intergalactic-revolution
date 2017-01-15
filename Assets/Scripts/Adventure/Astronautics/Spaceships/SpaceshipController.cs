82d8ca88bfa481ac25085990c36595b01d0fba42
Ben Scott <ias.jazz@gmail.com>
2017-01-14 22:20:50 -0500

    unified code, conditionally compile input

--- Assets/Scripts/Adventure/Astronautics/Spaceships/SpaceshipController.cs
+++ Assets/Scripts/Adventure/Astronautics/Spaceships/SpaceshipController.cs
@@ -13,8 +13,22 @@ namespace Adventure.Astronautics.Spaceships {
         public Spaceship Ship {get;set;}
         void Start() => Ship.ToggleView();
         void FixedUpdate() => Ship.Move(brake,boost,speed,roll,pitch,yaw);
-        void Update() {
-            if (!isLocalPlayer) return;
+        void Update() => If(isLocalPlayer, () => ControlInput());
+        void ControlInput() {
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+            // for use with xbox controllers
+            (roll, pitch) = (Input.GetAxis("Roll"),Input.GetAxis("Pitch"));
+            yaw = Input.GetAxis("Yaw");
+            speed = Input.GetAxis("Speed");
+            boost = Input.GetAxis("Boost");
+            if (boost<0) (boost,brake) = (0,boost);
+            if (Input.GetButton("Jump")) Ship.HyperJump();
+            if (Input.GetButton("Fire")) Ship.Fire();
+            if (Input.GetButtonDown("Switch")) Ship.SelectWeapon();
+            if (Input.GetButtonDown("Mode")) Ship.ChangeMode();
+            if (Input.GetButtonDown("Toggle")) Ship.ToggleView();
+            if (Input.GetButtonDown("Select")) Ship.SelectTarget();
+#else
             float AsAxis(string key) => Input.GetButton(key)?1:0;
             (roll, pitch) = (Input.GetAxis("Roll"),Input.GetAxis("Pitch"));
             (yaw, speed) = (Input.GetAxis("Yaw"), Input.GetAxis("Speed"));
@@ -25,6 +39,7 @@ namespace Adventure.Astronautics.Spaceships {
             if (Input.GetButtonDown("Mode")) Ship.ChangeMode();
             if (Input.GetButtonDown("Toggle")) Ship.ToggleView();
             if (Input.GetButtonDown("Select")) Ship.SelectTarget();
+#endif
         }
     }
 }
