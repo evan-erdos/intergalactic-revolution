0f2e4ff68e75aaaaebbae0dff9fe1135fe9f0fc2
Ben Scott <ias.jazz@gmail.com>
2017-01-30 23:56:10 -0500

    fixed awful textHUD performance mishap

--- Assets/Scripts/TextHUD.cs
+++ Assets/Scripts/TextHUD.cs
@@ -9,12 +9,13 @@ using Adventure.Astronautics.Spaceships;
 public class TextHUD : SpaceObject {
     enum ShipProperty { Mode, Speed, Cargo, System, Dest, Target, Weapon };
     [SerializeField] ShipProperty property = ShipProperty.Mode;
+    string content = "None";
     ui::Text text;
     Spaceship spaceship;
     void Start() => (text,spaceship) = (Get<ui::Text>(),GetParent<Spaceship>());
-    void Update() => text.text = $"{GetShipProperty(property)}";
-    string GetShipProperty(ShipProperty property) {
-        var s = "";
+    void OnEnable() => Loop(new WaitForSeconds(0.1f), () => Log());
+    void Log() { if (text.text!=content) text.text = $"{GetShipProperty(property)}"; }
+    string GetShipProperty(ShipProperty property, string s="") {
         switch (property) {
             case ShipProperty.Mode: s = $"{spaceship.Mode}"; break;
             case ShipProperty.Speed: s = $"{spaceship.ForwardSpeed} m/s"; break;
